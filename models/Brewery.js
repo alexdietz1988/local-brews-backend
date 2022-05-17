ac7f0d0ced110df1998fd99130b4654c69812787
alexdietz1988 <alex.dietz@gmail.com>
2022-05-17 19:32:48 -0400

    MyList displays data from database

--- models/Brewery.js
+++ models/Brewery.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const brewerySchema = new mongoose.Schema({
     username: {type: String, required: true},
-    brewery_id: {type: String, required: true},
+    id: {type: String, required: true},
+    name: {type: String, required: true},
     location: String
 })
 
