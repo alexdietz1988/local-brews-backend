7df99b8272ea6c63e01a0c6843e9cf151302e958
alexdietz1988 <alex.dietz@gmail.com>
2022-05-17 17:48:45 -0400

    Send form data to database

--- server.js
+++ server.js
@@ -12,26 +12,30 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.post('/new-user', async (req, res, next) => {
-    try {
-        await db.User.create(req.body)
-        return res.redirect('/')
-    } catch (error) {
-        req.error = error
-        console.log(error)
-        return next()
-    }
-})
-
-app.post('/add-brewery/', async (req, res, next) => {
     console.log(req.body)
     // try {
-    //     await db.Brewery.create(req.body)
+    //     await db.User.create(req.body)
     //     return res.redirect('/')
     // } catch (error) {
     //     req.error = error
     //     console.log(error)
     //     return next()
     // }
+})
+
+app.post('/add-brewery/:username/:city/:state/:id', async (req, res, next) => {
+    try {
+        await db.Brewery.create({
+            username: req.params.username,
+            brewery_id: req.params.id,
+            location: `${req.params.city}, ${req.params.state}`
+        })
+        return res.redirect('/')
+    } catch (error) {
+        req.error = error
+        console.log(error)
+        return next()
+    }
 })
 
 app.put('/update-brewery/:id', async (req, res, next) => {
