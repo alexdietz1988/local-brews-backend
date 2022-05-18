8d7010b7591cb308c322bf0d7df9267276658345
alexdietz1988 <alex.dietz@gmail.com>
2022-05-18 17:57:35 -0400

    Working on beer log

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,21 +2,38 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-router.get('/my-list/:user', async (req, res, next) => {
+router.get('/my-list/:username', async (req, res) => {
     try {
-        const myList = await db.Brewery.find({username: req.params.user})
-        res.send(myList)
+        const myList = await db.Brewery.find({username: req.params.username})
+        res.json(myList)
     } catch (error) {
-        req.error = error
-        console.log(error)
-        return next()
+        res.status(400).json(error)
+    }
+})
+
+router.get('/beer-log/:username', async (req, res) => {
+    try {
+        const beerLog = await db.Beer.find({username: req.params.username})
+        res.json(beerLog)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.get('/beer-log/:username/:brewery', async (req, res) => {
+    try {
+        const breweryBeerLog = await db.Beer.find({
+            username: req.params.username,
+            brewery_name: req.params.brewery})
+        res.json(breweryBeerLog)
+    } catch (error) {
+        res.status(400).json(error)
     }
 })
 
 router.post('/beer', async (req, res) => {  
     try {
-        console.log(req.body)
-        let newBeer = await db.Beer.create(req.body)
+        const newBeer = await db.Beer.create(req.body)
         res.json(newBeer)
     } catch (error) {
         res.status(400).json(error)
