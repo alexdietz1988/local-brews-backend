14673efd9d524f8cc968f5307526a38f324d5824
alexdietz1988 <alex.dietz@gmail.com>
2022-05-19 08:59:09 -0400

    Add more properties to beer

--- models/Beer.js
+++ models/Beer.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const beerSchema = new mongoose.Schema({
     username: String,
     brewery_id: String,
+    brewery_name: String,
+    brewery_location: String,
+
     name: {type: String, required: [true, 'no name given']},
     style: String,
     rating: String,
