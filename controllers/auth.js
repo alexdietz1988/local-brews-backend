a831e29aca14740a344a322ee72277d8ece8c0da
alexdietz1988 <alex.dietz@gmail.com>
2022-09-19 12:17:08 -0400

    Debugging login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
+const db = require('../models')
 
 router.post('/', async (req, res) => {
     try {
-        const foundUser = await User.exists({user: req.body.user})
+        const foundUser = await db.User.exists({user: req.body.user})
         if (foundUser) return res.json({ success: false, errorMessage: 'user already exists'})
 
         const salt = await bcrypt.genSalt(12)
         const hash = await bcrypt.hash(req.body.password, salt)
         req.body.password = hash
 
-        await User.create(req.body)
+        await db.User.create(req.body)
         return res.json({ success: true })
     } catch (error) {
         console.log(error)
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const foundUser = await User.findOne({ user: req.body.user })
+        const foundUser = await db.User.findOne({ user: req.body.user })
         const match = await bcrypt.compare(req.body.password, foundUser.password)
         if (!match) return res.json({success: false, errorMessage: 'invalid username or password'})
         return res.json({ success: true })
