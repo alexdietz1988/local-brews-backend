6ef73ce11cd7e65b9f1d430e27c1a9b41c5009f4
alexdietz1988 <alex.dietz@gmail.com>
2022-09-20 15:01:32 -0400

    Debugging

--- controllers/breweries.js
+++ controllers/breweries.js
@@ -12,6 +12,7 @@ router.get('/:user', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+    console.log(req.body)
     try {
         await db.Brewery.create(req.body)
         res.json({ success: true })
