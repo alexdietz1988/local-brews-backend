647845e10a169a2f4c43a0080369bd653089bee2
alexdietz1988 <alex.dietz@gmail.com>
2022-09-21 15:10:22 -0400

    Debugging login

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,9 @@ const db = require('../models')
 router.post('/', async (req, res) => {
     try {
         const foundUser = await db.User.exists({user: req.body.user})
-        if (foundUser) return res.json({ success: false, errorMessage: 'user already exists'})
+        if (foundUser) return res.json({
+            success: false,
+            errorMessage: 'user already exists'})
 
         const salt = await bcrypt.genSalt(12)
         const hash = await bcrypt.hash(req.body.password, salt)
@@ -25,7 +27,10 @@ router.post('/login', async (req, res) => {
     try {
         const foundUser = await db.User.findOne({ user: req.body.user })
         const match = await bcrypt.compare(req.body.password, foundUser.password)
-        if (!match) return res.json({success: false, errorMessage: 'invalid username or password'})
+        if (!match) return res.json({
+            success: false,
+            errorMessage: 'invalid username or password'
+        })
         return res.json({ success: true })
     } catch (error) {
         console.log(error)
